196eea330e433797d072766d99fb87dde5f1f468
Karol Majerczyk <karol.majerczyk@gmail.com>
2025-01-16 20:24:51 +0100

    chore: something 41

--- scripts/features/budget/handlers/handleBudgetSectionRender.js
+++ scripts/features/budget/handlers/handleBudgetSectionRender.js
@@ -3,5 +3,6 @@ import { renderBudgetItems } from "../budgetView.js";
 
 export function handleBudgetSectionRender(obj) {
   setActiveObject(obj);
+  document.querySelector(".budget #dropdown").selectedIndex = 0;
   renderBudgetItems(obj.budget);
 }
