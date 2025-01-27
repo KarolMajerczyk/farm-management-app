e2570e4225a2a9e78d0ef065be440a93520d38c6
Karol Majerczyk <karol.majerczyk@gmail.com>
2025-01-27 20:11:16 +0100

    chore: something 60

--- scripts/features/content/contentController.js
+++ scripts/features/content/contentController.js
@@ -16,23 +16,25 @@ export function initContentController() {
       .addEventListener("click", handleAnimalItemAdd);
   }
 
-  document.querySelector("#content-list").addEventListener("click", (e) => {
-    if (e.target.classList.contains("btn-delete")) {
-      handleAnimalItemDelete(e);
-    }
+  if (document.querySelector("#content-list")) {
+    document.querySelector("#content-list").addEventListener("click", (e) => {
+      if (e.target.classList.contains("btn-delete")) {
+        handleAnimalItemDelete(e);
+      }
 
-    if (e.target.classList.contains("btn-edit")) {
-      handleContentRender("edit", e);
-    }
+      if (e.target.classList.contains("btn-edit")) {
+        handleContentRender("edit", e);
+      }
 
-    if (e.target.classList.contains("btn-close")) {
-      handleContentRender();
-    }
-  });
+      if (e.target.classList.contains("btn-close")) {
+        handleContentRender();
+      }
+    });
 
-  document.querySelector("#content-list").addEventListener("submit", (e) => {
-    handleAnimalItemUpdate(e);
-  });
+    document.querySelector("#content-list").addEventListener("submit", (e) => {
+      handleAnimalItemUpdate(e);
+    });
+  }
 
   // if (document.querySelector("#content")) {
   //   document.querySelector("#content").addEventListener("dragover", (e) => {
